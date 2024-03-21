9326026c5b9fb41957e445c15216a2e06545c459
pyloverm <ferreirabictor74200@gmail.com>
2024-03-21 14:26:12 +0100

    Supprimer la fonction useTranslations de utils.ts

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -5,9 +5,3 @@ export function getLangFromUrl(url: URL) {
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
-
-export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
-    return ui[lang][key] || ui[defaultLang][key];
-  };
-}
