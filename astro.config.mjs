b99b452b236c4a6a6e7dee791126b71823747efa
pyloverm <ferreirabictor74200@gmail.com>
2024-03-28 02:22:51 +0100

    Mise à jour des fichiers de configuration et des styles globaux

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,28 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: import.meta.env.DEV
     ? "http://localhost:4321"
     : "https://luna-landing-rust.vercel.app/",
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    robotsTxt(),
+    react(),
+  ],
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true },
+    webAnalytics: {
+      enabled: true,
+    },
   }),
 });
